58012c9baea5021a1d7adcb0ed582ed5bc789ab2
akhamat cool <newbie.hao.semarang@gmail.com>
2018-02-21 22:52:38 +0700

    commit this

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Transformers\ProductTransformer;
 use Kodami\Models\Mysql\Member;
+use Kodami\Models\Mysql\CategorySpesification;
+use Kodami\Models\Mysql\Category;
+use Kodami\Models\Mysql\JunkCategorySpesification;
 use Kodami\Models\Mysql\Product;
 use Kodami\Models\Test;
 
 class ExampleController extends ApiController
 {
     public function index()
-    {
-    	$data = Product::orderBy('id', 'desc')->first();
+    {            	
+        $data = Product::orderBy('id', 'desc')->first();
     	return $this->response()->success($data, ['meta.token' => ''] , 200, new ProductTransformer(), 'item', null, ['criteria']);
     }
 
