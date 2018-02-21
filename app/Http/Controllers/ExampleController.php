b979ce4187b6751343968ef9e4fcd1f284c2cda0
akhamat cool <newbie.hao.semarang@gmail.com>
2018-02-21 23:17:44 +0700

    push this

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -6,14 +6,29 @@ use App\Transformers\ProductTransformer;
 use Kodami\Models\Mysql\Member;
 use Kodami\Models\Mysql\CategorySpesification;
 use Kodami\Models\Mysql\Category;
+use Kodami\Models\Mysql\CategoryCriteria;
 use Kodami\Models\Mysql\JunkCategorySpesification;
+use Kodami\Models\Mysql\JunkCategoryCriteria;
 use Kodami\Models\Mysql\Product;
 use Kodami\Models\Test;
 
 class ExampleController extends ApiController
 {
     public function index()
-    {            	
+    {
+        $p = CategoryCriteria::get();
+        $c = Category::get();
+
+        foreach ($p as $key => $value) {
+            foreach ($c as $key2 => $value2) {
+                $d = new JunkCategoryCriteria;
+                $d->category_id = $value2->id;
+                $d->category_criteria_id = $value->id;
+
+                $d->save();
+            }
+        }
+
         $data = Product::orderBy('id', 'desc')->first();
     	return $this->response()->success($data, ['meta.token' => ''] , 200, new ProductTransformer(), 'item', null, ['criteria']);
     }
