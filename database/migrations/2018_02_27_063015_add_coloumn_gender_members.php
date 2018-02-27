fdaf2fd9979b96d4e95e2ea3c475f11c6f073b3e
akhamat cool <newbie.hao.semarang@gmail.com>
2018-02-27 12:33:37 +0700

    commit this

--- database/migrations/2018_02_27_063015_add_coloumn_gender_members.php
+++ database/migrations/2018_02_27_063015_add_coloumn_gender_members.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColoumnLongDescription extends Migration
+class AddColoumnGenderMembers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AddColoumnLongDescription extends Migration
      */
     public function up()
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->text('long_description')->after('description');
+        Schema::table('members', function (Blueprint $table) {
+            $table->boolean('gender')->default(1)->after('name');
         });
     }
 
@@ -25,8 +25,8 @@ class AddColoumnLongDescription extends Migration
      */
     public function down()
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('long_description');
+        Schema::table('members', function (Blueprint $table) {
+            $table->dropColumn('gender');
         });
     }
 }
