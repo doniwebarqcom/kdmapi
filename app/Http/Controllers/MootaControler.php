c10cdfc6484e587bcf44315d5d5421954d16ab94
Ramdoni <doni.enginer@gmail.com>
2018-11-01 16:43:05 +0700

    Response Moota

---
+++ app/Http/Controllers/MootaControler.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Pulsa;
+
+use App\Http\Controllers\ApiController;
+use Tymon\JWTAuth\JWTAuth;
+use Kodami\Models\Mysql\PPulsaTransaksi;
+use Kodami\Models\Mysql\UserDropshiper;
+use Kodami\Models\Mysql\Mutation;
+use Kodami\Models\Mysql\PInvoice;
+use Kodami\Models\Mysql\UserDropshiperHistoryKuota;
+
+class IndexController extends ApiController
+{
+	/**
+	 * Index Response
+	 * @param  JWTAuth $JWTAuth [description]
+	 * @return [type]           [description]
+	 */
+    public function index(JWTAuth $JWTAuth)
+    {
+        $response['status'] = 'success';
+        $response['code'] = '200';
+        $mutasi      = json_encode($this->request->all());
+
+        $temp = \Kodami\Models\Mysql\Mutation::where('mutation_id', $mutasi->mutation_id)->first();
+
+        if(!$temp)
+        {
+            $temp                   = new \Kodami\Models\Mysql\Mutation();
+            $temp->rekening_bank_id = $bank->id;
+            $temp->date_transfer    = $mutasi->date;
+            $temp->description      = $mutasi->description;
+            $temp->amount           = $mutasi->amount;
+            $temp->type             = $mutasi->type == 'DB' ? 2 : 1;
+            $temp->note             = $mutasi->note;
+            $temp->account_number   = $mutasi->account_number;
+            $temp->mutation_id      = $mutasi->mutation_id;
+            $temp->created_at_mutation=$mutasi->created_at;
+            $temp->save();
+
+            echo " DATE TRANSFER : ". strtoupper($bank->atas_nama) ." \n";
+            echo " DESCRIPTION : ". strtoupper($mutasi->description) ." \n";
+            echo " AMOUNT : ". strtoupper($mutasi->amount) ." \n";
+            echo " TYPE : ". strtoupper($mutasi->type) ." \n\n";
+
+            $invoice = PInvoice::where('status',2)->where('jenis_pembayaran', 1)->whereNotNull('unique')->get();
+            foreach($invoice as $item)
+            {
+              if($item->nominal == $mutasi->amount)
+              {
+                $data                 = PInvoice::where('id',$item->id)->first();
+                $data->mutation_id    = $temp->id;
+                $data->status = 3;
+                $data->save();
+
+                PPulsaTransaksi::where('invoice_id', $item->id)->update(['status_pembayaran' => 2]);
+                echo " ==========================================================\n";
+                echo " NO INVOICE : ". strtoupper($item->no_invoice) ." \n";
+                echo " ==========================================================\n\n";
+
+                // UPDATE KUOTA
+                $kuota                = UserDropshiper::where('user_id', $item->user_id)->first();
+                $kuota->saldo         = $kuota->saldo + ($mutasi->amount - $item->unique);
+                $kuota->saldo_terpakai= $kuota->saldo_terpakai - ($mutasi->amount - $item->unique);
+                $kuota->save();
+
+                // HISTORY KUOTA
+                $history                    = new UserDropshiperHistoryKuota();
+                $history->user_id           = $item->user_id;
+                $history->user_proses_id    = 0;
+                $history->nominal           = $mutasi->amount;
+                $history->type              = 2; // topup by transfer invoice
+                $history->save();
+              }
+            }         
+        }
+
+        $response['data_from_moota'] = $mutasi;
+
+        return $this->response()->success($response);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -34,43 +34,29 @@ $router->group(['namespace' => 'Pulsa', 'prefix' => 'pulsa'], function() use($ro
 	$router->post('response', 'IndexController@response_post');
 	$router->get('response', 'IndexController@response_get');
 });
-
-$router->get('/', [
-  'uses' => 'ExampleController@index'
-]);
-
+$router->post('moota/response', 'MootaController@index');
+$router->get('/', ['uses' => 'ExampleController@index']);
 $router->get('rajaongkir', 'ExampleController@rajaongkir');
 $router->get('rajaongkir/province', 'ExampleController@rajaongkirProvince');
 $router->get('rajaongkir/city', 'ExampleController@rajaongkirCity');
-
 $router->get('transaction/{transaction_code}/detail', ['uses' => 'MemberController@detail_transaction' , 'middleware' => ['cors', 'jwtauth']]);
 $router->get('transaction/list', ['uses' => 'MemberController@list_transaction' , 'middleware' => ['cors', 'jwtauth']]);
 $router->get('pending/top_up', ['uses' => 'MemberController@pending_top_up' , 'middleware' => ['cors', 'jwtauth']]);
-
 $router->get('special-offer', 'BannerController@special_offer');
 $router->get('wishlist', ['uses' => 'WishlistController@list' , 'middleware' => ['cors', 'jwtauth']]);
 $router->delete('wishlist', ['uses' => 'WishlistController@destroy' , 'middleware' => ['cors', 'jwtauth']]);
 $router->post('wishlist', ['uses' => 'WishlistController@add' , 'middleware' => ['cors', 'jwtauth']]);
-
 $router->post('isi/saldo', ['uses' => 'MemberController@isi_saldo' , 'middleware' => ['cors', 'jwtauth']]);
-
 $router->get('search/product/{category}', 'ProductController@category');
 $router->get('suggest/product', 'ProductController@suggest');
-
 $router->post('shipping', 'ShippingController@getData');
-
 $router->get('ads-home', ['uses' => 'BannerController@ads', 'middleware' => ['cors']]);
-
 $router->get('checkout', ['uses' => 'CheckoutController@store', 'middleware' => ['cors', 'jwtauth']]);
-
 $router->get('product/most-viewed', ['uses' => 'ProductController@most_viewed', 'middleware' => ['cors']]);
-
 $router->get('banner_slideshow', ['uses' => 'BannerController@slideshow', 'middleware' => ['cors']]);
 $router->get('our_product', ['uses' => 'BannerController@our_product', 'middleware' => ['cors']]);
 $router->get('category_home', ['uses' => 'BannerController@category_home', 'middleware' => ['cors']]);
-
 $router->get('user/info', ['uses' => 'MemberController@getUser', 'middleware' => ['cors', 'jwtauth']]);
-
 $router->get('member/dana/simpanan_anggota', ['uses' => 'MemberController@dana_simpanan_anggota' , 'middleware' => ['cors', 'jwtauth']]);
 $router->get('member/koprasi/product/validated', ['uses' => 'MemberController@product_validated' , 'middleware' => ['cors', 'jwtauth']]);
 $router->get('member/place/list', ['uses' => 'MemberController@place_list' , 'middleware' => ['cors', 'jwtauth']]);
@@ -86,46 +72,33 @@ $router->post('member/edit/profile', ['uses' => 'MemberController@profile_store'
 $router->post('member/place', ['uses' => 'Member\PlaceController@store' , 'middleware' => ['cors', 'jwtauth']]);
 $router->put('member/place', ['uses' => 'Member\PlaceController@put' , 'middleware' => ['cors', 'jwtauth']]);
 $router->delete('member/place', ['uses' => 'Member\PlaceController@destroy' , 'middleware' => ['cors', 'jwtauth']]);
-
 $router->post('subscribe', ['uses' => 'SubscriberController@index', 'middleware' => ['cors']]);
 $router->post('unsubscribe', ['uses' => 'SubscriberController@delete', 'middleware' => ['cors']]);
-
 $router->get('category', ['uses' => 'CategoryController@index', 'middleware' => ['cors']]);
 $router->get('category-insearch', ['uses' => 'CategoryController@search', 'middleware' => ['cors']]);
-
 $router->post('shop/register', ['uses' => 'ShopController@register', 'middleware' => ['cors', 'jwtauth']]);
-
 $router->post('product/input', ['uses' => 'ProductController@input', 'middleware' => ['cors', 'jwtauth']]);
 $router->get('product/list', ['uses' => 'ProductController@list', 'middleware' => ['cors', 'jwtauth']]);
-
 $router->get('place/province', ['uses' => 'PlaceController@getProvince', 'middleware' => ['cors']]);
 $router->get('place/regency', ['uses' => 'PlaceController@getRegency', 'middleware' => ['cors']]);
 $router->get('place/district', ['uses' => 'PlaceController@getDistrict', 'middleware' => ['cors']]);
 $router->get('place/village', ['uses' => 'PlaceController@getVillage', 'middleware' => ['cors']]);
 $router->get('place/postal', ['uses' => 'PlaceController@getPostal', 'middleware' => ['cors']]);
 $router->get('place/postal-code/district', ['uses' => 'PlaceController@postalcodeByDistrict', 'middleware' => ['cors']]);
-
 $router->post('image/upload', ['uses' => 'ImageController@upload', 'middleware' => ['cors']]);
-
 $router->get('criteria/category', ['uses' => 'CriteriaController@category', 'middleware' => ['cors']]);
 $router->get('spesification/category', ['uses' => 'CriteriaController@spesification', 'middleware' => ['cors']]);
-
 $router->get('sms', ['uses' => 'ExampleController@sms', 'middleware' => ['cors']]);
-
 $router->get('ocupation', ['uses' => 'OcupationController@index', 'middleware' => ['cors']]);
 $router->get('selling/enviroment', ['uses' => 'OcupationController@sellEnv', 'middleware' => ['cors']]);
-
 $router->post('register/dropshiper', ['uses' => 'DropshiperController@store', 'middleware' => ['cors', 'jwtauth']]);
-
 $router->get('cart', ['uses' => 'CartController@list' , 'middleware' => ['cors', 'jwtauth']]);
 $router->post('cart/update', ['uses' => 'CartController@update' , 'middleware' => ['cors', 'jwtauth']]);
 $router->post('cart/store', ['uses' => 'CartController@store' , 'middleware' => ['cors', 'jwtauth']]);
 $router->post('cart/store/withNewPlace', ['uses' => 'CartController@withNewPlace' , 'middleware' => ['cors', 'jwtauth']]);
 $router->delete('cart/{id}', ['uses' => 'CartController@destroy_cart' , 'middleware' => ['cors', 'jwtauth']]);
 $router->post('{product}/add-cart', ['uses' => 'CartController@addCart' , 'middleware' => ['cors', 'jwtauth']]);
-
 $router->post('payment/choose', ['uses' => 'PaymentController@choose' , 'middleware' => ['cors', 'jwtauth']]);
 $router->get('payment/bill/{invoice}', ['uses' => 'PaymentController@bill' , 'middleware' => ['cors', 'jwtauth']]);
-
 $router->get('product/{alias}', 'ProductController@getData');
 $router->get('{koprasi}/{product}', 'ProductController@single');
